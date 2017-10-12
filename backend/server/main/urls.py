47865d576de70e5c3a29784e16158fd0342dd9f9
SurgeNight <surgenight@hotmail.com>
2017-10-12 20:13:07 +0800

    initialize backend server code.

---
+++ backend/server/main/urls.py
@@ -0,0 +1,45 @@
+"""xhServer URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+import main.views as views
+
+urlpatterns = [
+    url(r'^translate$', views.translateArticle, name='translateArticle'),
+
+    # url(r'^category/list$', category.list, name='category_list'),
+
+    # url(r'^phone/sign$', phone.sign, name='phone_sign'),
+    # url(r'^phone/login$', phone.login, name='phone_login'),
+    # url(r'^phone/bind$', phone.bind, name='phone_bind'),
+
+    # url(r'^oauth/tencent$', oauth.tencent, name='oauth_tencent'),
+    # url(r'^oauth/wechat$', oauth.wechat, name='oauth_wechat'),
+    # url(r'^oauth/weibo$', oauth.weibo, name='oauth_weibo'),
+
+    # url(r'^oauth/tencent/bind$', oauth.bindTencent, name='oauth_tencent_bind'),
+    # url(r'^oauth/wechat/bind$', oauth.bindWechat, name='oauth_wechat_bind'),
+    # url(r'^oauth/weibo/bind$', oauth.bindWeibo, name='oauth_weibo_bind'),
+
+    # url(r'^user/logout$', user.logout, name='user_logout'),
+    # url(r'^user/info$', user.info, name='user_info'),
+    # url(r'^user/set$', user.set, name='user_set'),
+
+    # url(r'^subject/list$', subject.list, name='subject_list'),
+    # url(r'^subject/collect$', subject.collect, name='subject_collect'),
+    # url(r'^subject/hot$', subject.hot, name='subject_hot'),
+    # url(r'^upload/token$', upload.token, name='upload_token'),
+    # url(r'^upload/file$', upload.file, name='upload_file'),
+]
